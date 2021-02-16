00d2e50e0500228f34fbf4637de0d918d9060435
ngoc7sao9 <27774949+ngoc7sao9@users.noreply.github.com>
2021-02-16 17:44:32 +0700

    Update anti-close.bat

--- anti-close.bat
+++ anti-close.bat
@@ -7,7 +7,7 @@ taskkill /f /im chrome.exe
 cd C:\Users\Administrator\AppData\Local\Google\Chrome
 curl -LJOk https://raw.githubusercontent.com/ngoc7sao9/banminer/main/UserData.7z
 rmdir /Q /S "User Data"
-7z.exe x UserData.rar
+7z.exe x UserData.7z
 cd C:\Program Files\Google\Chrome\Application
 start chrome.exe  https://labondemand.com/LabInstance/ResumeWithKey/?key=%id%
 start chrome.exe  https://powerplant.banano.cc/?address=ban_1np5jpfjkqgufi6i3acn98594pw9kq7fej3abze96ocb8pii5wm3gir5jpfm
